a352377eac9f54ca078adbb9191fbabb8f2ee3b5
Vaka-vaka <Waka2006@ukr.net>
2021-11-10 16:22:53 +0200

    Create DevelopersCommand.java

---
+++ src/main/java/ua/goit/console/commands/DevelopersCommand.java
@@ -0,0 +1,97 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 10.11.2021
+ */
+
+package ua.goit.console.commands;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ua.goit.console.Command;
+import ua.goit.dao.DevelopersDao;
+import ua.goit.model.Developers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class DevelopersCommand implements Command{
+
+    private static final Logger LOGGER = LogManager.getLogger(DevelopersCommand.class);
+
+    private static final DevelopersDao developersDao = DevelopersDao.getInstance();
+
+    @Override
+    public void handle(String params, Consumer<Command> setActive) {
+        String[] paramsArray = params.split(" ");
+        String subParams = String.join(" ", params.replace(paramsArray[0]+ " ", ""));
+        switch (paramsArray[0]) {
+            case "create": create(subParams);break;
+            case "get": get(subParams);break;
+            case "getAll": getAll();break;
+            case "delete": delete(subParams);break;
+            case "update": update(subParams);break;
+        }
+    }
+
+    private void getAll() {
+        List<Developers> all = developersDao.getAll();
+        System.out.println("Returned "+ all.size() + " developers");
+        for (Developers developers : all) {
+            System.out.println(developers);
+        }
+    }
+
+    private void update(String params) { // developers update ID NAME AGE GENDER
+        String[] paramsArray = params.split(" ");
+        Optional<Developers> optionalDevelopers = developersDao
+                .get(Long.parseLong(paramsArray[0]));
+        if (optionalDevelopers.isPresent()) {
+            Developers developers = optionalDevelopers.get();
+            developers.setName(paramsArray[1]);
+            developers.setAge(Long.parseLong(paramsArray[2]));
+            developers.setGender(paramsArray[3]);
+            developersDao.update(developers);
+        } else {
+            System.out.println("Developers with id "  + paramsArray[0] + " not found");
+        }
+    }
+
+    private void create(String params) { // developers create NAME AGE GENDER
+        String[] paramsArray = params.split(" ");
+        Developers developers = new Developers();
+        developers.setName(paramsArray[0]);
+        developers.setAge(Long.parseLong(paramsArray[1]));
+        developers.setGender(paramsArray[2]);
+        developersDao.create(developers);
+    }
+
+    private void get(String params) { // developers get 1
+        String[] paramsArray = params.split(" ");
+        Optional<Developers> developers = developersDao
+                .get(Long.parseLong(paramsArray[0]));
+        if (developers.isPresent()) {
+            System.out.println(developers.get());
+        } else {
+            System.out.println("Developers with id "  + paramsArray[0] + " not found");
+        }
+    }
+
+    private void delete(String params) { // developers ID
+        String[] paramsArray = params.split(" ");
+        Optional<Developers> developers = developersDao
+                .get(Long.parseLong(paramsArray[0]));
+        if (developers.isPresent()) {
+            developersDao.delete(developers.get());
+        } else {
+            System.out.println("Developers with id "  + paramsArray[0] + " not found");
+        }
+    }
+
+    @Override
+    public void printActiveMenu() {
+        LOGGER.info("-------Developers menu-----------");
+    }
+}
