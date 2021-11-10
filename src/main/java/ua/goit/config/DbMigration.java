d801aa3b04069d12fb35167e404e3939b0cb83d5
Vaka-vaka <Waka2006@ukr.net>
2021-11-10 16:22:43 +0200

    Create DbMigration.java

---
+++ src/main/java/ua/goit/config/DbMigration.java
@@ -0,0 +1,20 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 10.11.2021
+ */
+
+package ua.goit.config;
+
+import org.flywaydb.core.Flyway;
+
+public class DbMigration {
+
+    public static void migrate() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(DataSourceHolder.getDataSource())
+                .load();
+        flyway.migrate();
+    }
+}
