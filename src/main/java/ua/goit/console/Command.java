cb2b7d13458437c410867cca198e4821d963e032
Vaka-vaka <Waka2006@ukr.net>
2021-11-10 16:22:47 +0200

    Create Command.java

---
+++ src/main/java/ua/goit/console/Command.java
@@ -0,0 +1,29 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 10.11.2021
+ */
+
+package ua.goit.console;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public interface Command {
+
+    Pattern pattern = Pattern.compile("^\\w+");
+
+    void handle(String params, Consumer<Command> setActive);
+    void printActiveMenu();
+    default Optional<String> getCommandString(String params) {
+        Matcher matcher = pattern.matcher(params);
+        if (matcher.find()) {
+            return Optional.of(matcher.group());
+        } else {
+            return Optional.empty();
+        }
+    }
+}
