d704884e18e361ca892d575726225c1d7a97ce4c
Vaka-vaka <Waka2006@ukr.net>
2021-11-10 11:26:19 +0200

    Create User.java

---
+++ src/main/java/ua/goit/model/User.java
@@ -0,0 +1,54 @@
+package ua.goit.model;
+
+
+import ua.goit.dao.Identity;
+
+import java.util.List;
+
+public class User implements Identity {
+    private Long id;
+    private String name;
+    private String description;
+    private List<Group> groups;
+
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
