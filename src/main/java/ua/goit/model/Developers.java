e52145733e33159dd10fcde356a0c16e2761e4ad
Vaka-vaka <Waka2006@ukr.net>
2021-11-10 14:51:57 +0200

    Create Developers.java

---
+++ src/main/java/ua/goit/model/Developers.java
@@ -0,0 +1,60 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 10.11.2021
+ */
+
+package ua.goit.model;
+
+import ua.goit.dao.interfaces.Identity;
+
+public class Developers implements Identity {
+
+    private Long id;
+    private String name;
+    private Long age;
+    private String gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Developers{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
