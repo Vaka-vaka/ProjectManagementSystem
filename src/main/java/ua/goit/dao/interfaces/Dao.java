e3ffb0bfbddbdd88e41a32e4fcddef9fd9333646
Vaka-vaka <Waka2006@ukr.net>
2021-11-10 17:06:15 +0200

    Create Dao.java

---
+++ src/main/java/ua/goit/dao/interfaces/Dao.java
@@ -0,0 +1,24 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 10.11.2021
+ */
+
+package ua.goit.dao.interfaces;
+
+import ua.goit.dao.interfaces.Identity;
+
+import java.util.*;
+
+public interface Dao<T extends Identity> {
+    List<T> getAll();
+
+    Optional<T> get(Long id);
+
+    Optional<T> create(T entity);
+
+    void update(T entity);
+
+    void delete(T entity);
+}
