1e37a2b69ed65b5485392cbf9d1af1233ac14869
Vaka-vaka <Waka2006@ukr.net>
2021-11-10 14:51:53 +0200

    Create Identity.java

---
+++ src/main/java/ua/goit/dao/interfaces/Identity.java
@@ -0,0 +1,13 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 10.11.2021
+ */
+
+package ua.goit.dao.interfaces;
+
+public interface Identity {
+
+    Long getId();
+}
