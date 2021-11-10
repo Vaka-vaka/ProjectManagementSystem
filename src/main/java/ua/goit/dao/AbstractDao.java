9f2e0d063483349a1c6cfa9492ff9c84499d685d
Vaka-vaka <Waka2006@ukr.net>
2021-11-10 16:23:01 +0200

    Update AbstractDao.java

--- src/main/java/ua/goit/dao/AbstractDao.java
+++ src/main/java/ua/goit/dao/AbstractDao.java
@@ -7,6 +7,8 @@
 
 package ua.goit.dao;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ua.goit.dao.interfaces.Identity;
 
 import java.sql.ResultSet;
@@ -15,32 +17,54 @@ import java.util.*;
 
 abstract class AbstractDao<T extends Identity> implements Dao<T> {
 
-    abstract String getTableName();
+    private static final Logger LOGGER = LogManager.getLogger(AbstractDao.class);
 
-    abstract T mapToEntity(ResultSet resultSet) throws SQLException;
+    abstract String getTableName();
+    abstract T mapToEntity(ResultSet rs) throws SQLException;
 
     @Override
-    public List<T> getAll() {
-        return null;
+    public void delete(T entity) {
+        String sql = String.format("delete from %s where id = ?", getTableName());
+        DbHelper.executeWithPreparedStatement(sql, ps -> {
+            ps.setLong(1, entity.getId());
+        });
+        LOGGER.debug("Deleted record from " + getTableName());
     }
 
     @Override
     public Optional<T> get(Long id) {
+        String query = String.format("select * from %s where id = ?", getTableName());
+        try {
+            ResultSet resultSet = DbHelper.getWithPreparedStatement(
+                    query, ps -> {
+                        ps.setLong(1, id);
+                    });
+            if (resultSet.next()) {
+                LOGGER.debug("Record was selected");
+                return Optional.of(mapToEntity(resultSet));
+            } else {
+                return Optional.empty();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return Optional.empty();
     }
 
     @Override
-    public Optional<T> create(T entity) {
-        return Optional.empty();
-    }
-
-    @Override
-    public void update(T entity) {
-
-    }
-
-    @Override
-    public void delete(T entity) {
-
+    public List<T> getAll() {
+        List<T> resultList = new ArrayList<>();
+        String query = String.format("select * from %s", getTableName());
+        try {
+            ResultSet resultSet = DbHelper.getWithPreparedStatement(
+                    query, ps -> {
+                    });
+            while (resultSet.next()) {
+                resultList.add(mapToEntity(resultSet));
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Get all method exception", e);
+        }
+        return resultList;
     }
 }
