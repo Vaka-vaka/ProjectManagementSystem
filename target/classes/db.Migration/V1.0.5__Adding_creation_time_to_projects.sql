b256b32e341dfcf4496a19bc501c8f1b16038e28
Vaka-vaka <Waka2006@ukr.net>
2021-11-11 10:42:40 +0200

    del

--- target/classes/db.Migration/V1.0.5__Adding_creation_time_to_projects.sql
+++
@@ -1,5 +0,0 @@
-alter table projects add column creation_date DATE;
-
-update projects set creation_date = '2020.01.01' where id = 1;
-update projects set creation_date = '2021.10.15' where id = 2;
-update projects set creation_date = '2019.07.24' where id = 3;
