006d600896b917df4c027d0ae93f17099f44459c
Vaka-vaka <Waka2006@ukr.net>
2021-11-12 15:12:00 +0200

    Create Main.java

---
+++ src/main/java/ua/goit/Main.java
@@ -0,0 +1,25 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 02.11.2021
+ */
+
+package ua.goit;
+
+import ua.goit.console.CommandHandler;
+import java.util.*;
+
+public class Main {
+
+        public static void main (String[]args){
+
+            System.out.println("Start application");
+            CommandHandler commandHandler = new CommandHandler();
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()) {
+                commandHandler.handleCommand(scanner.nextLine());
+            }
+            System.out.println("And application");
+        }
+    }
