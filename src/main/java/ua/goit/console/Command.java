3aa17b50e65768970499e3facef009bfd3ea37e8
Vaka-vaka <Waka2006@ukr.net>
2021-11-12 15:11:09 +0200

    Create Command.java

---
+++ src/main/java/ua/goit/console/Command.java
@@ -0,0 +1,13 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 05.11.2021
+ */
+
+package ua.goit.console;
+
+public interface Command {
+
+    void handle(String params);
+}
