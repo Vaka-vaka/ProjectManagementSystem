336bac6200be47adb5942bff84e877f1f764b036
Vaka-vaka <Waka2006@ukr.net>
2021-11-14 21:58:54 +0200

    Update HomeWork.java

--- src/main/java/ua/goit/console/commands/HomeWork.java
+++ src/main/java/ua/goit/console/commands/HomeWork.java
@@ -19,12 +19,15 @@ public class HomeWork implements Command {
         String[] paramsArray = params.split(" ");
         String subParams = String.join(" "
                 , params.replace(paramsArray[0] + " ", ""));
-        if ("javaDevelopers".equals(paramsArray[0])) {
-            javaDevelopers();
+        switch (paramsArray[0]) {
+            case "javaDevelopers" : javaDevelopers();
+            break;
+            case "middleDevelopers" : middleDevelopers();
+            break;
         }
     }
 
-    public static void javaDevelopers() throws SQLException {
+    private static void javaDevelopers() throws SQLException {
         Connection connection = DataSourceHolder.getDataSource().getConnection();
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("select * from list_java_developers;");
@@ -34,5 +37,14 @@ public class HomeWork implements Command {
         connection.close();
     }
 
-    
+    private static void middleDevelopers() throws SQLException {
+        Connection connection = DataSourceHolder.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from  list_middleDevelopers;");
+        while (resultSet.next()) {
+            System.out.println("Middle developer: " + resultSet.getString(1));
+        }
+        connection.close();
+    }
+
 }
