fa33206a4eb73bb0af8b576067c2ee46a8504212
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2021-11-14 22:00:30 +0200

    Delete V1.0.5__Adding_creation_view_to_list_java_developers.sql

--- target/classes/db/migration/V1.0.5__Adding_creation_view_to_list_java_developers.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW list_java_developers AS
-select trim(d.name_ || ' ' || d.age || ' ' || d.gender || ' ' || d.salary)
-FROM developers d
-INNER JOIN developers_skills ds ON ds.dev_id=d.id
-INNER JOIN skills s ON s.id = ds.skills_id
-WHERE s.language = 'java';
