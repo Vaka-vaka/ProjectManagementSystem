8c10ae2d60d839bd95d0846c2091f0c3c1e129f0
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2021-11-15 15:31:23 +0200

    Delete V1.0.8__Adding_creation_list_projects.sql

--- target/classes/db/migration/V1.0.8__Adding_creation_list_projects.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW list_projects AS
-SELECT count (dp.dev_id) || ' - ' || p.name_ || ' - ' || to_char (p.creation_date, 'dd.mm.yyyy') as list_project
-FROM projects p
-JOIN developers_projects dp on dp.projects_id = p.id
-GROUP BY creation_date, p.name_;
