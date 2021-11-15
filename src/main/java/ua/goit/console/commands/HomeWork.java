9c30162ab9d331c96d33288b2e73131cd3bd6552
Vaka-vaka <Waka2006@ukr.net>
2021-11-15 12:48:38 +0200

    Update HomeWork.java

--- src/main/java/ua/goit/console/commands/HomeWork.java
+++ src/main/java/ua/goit/console/commands/HomeWork.java
@@ -9,6 +9,7 @@ package ua.goit.console.commands;
 
 import ua.goit.config.DataSourceHolder;
 import ua.goit.console.Command;
+
 import java.sql.Connection;
 import java.sql.*;
 
@@ -20,12 +21,21 @@ public class HomeWork implements Command {
         String subParams = String.join(" "
                 , params.replace(paramsArray[0] + " ", ""));
         switch (paramsArray[0]) {
-            case "salary" : getSumProjectSalary();
+            case "salary":
+                getSumProjectSalary();
                 break;
-            case "developers" : getJavaDevelopers();
-            break;
-            case "developer" : getMiddleDevelopers();
-            break;
+            case "project":
+                getListDevelopersProject();
+                break;
+            case "developers":
+                getJavaDevelopers();
+                break;
+            case "developer":
+                getMiddleDevelopers();
+                break;
+            case "go":
+                getListDevelopers();
+                break;
         }
     }
 
@@ -38,6 +48,23 @@ public class HomeWork implements Command {
         while (resultSet.next()) {
             System.out.println("Project name: " + resultSet.getString(1) +
                     " ; " + "Sum salary developer: " + resultSet.getString(2));
+        }
+        connection.close();
+    }
+
+    private void getListDevelopersProject() throws SQLException { // developer project
+        Connection connection = DataSourceHolder.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement
+                .executeQuery("select p.name_, trim(d.name_ || ' ' || d.age) as d_name_ \n" +
+                        "from projects p\n" +
+                        "join developers_projects dp on dp.projects_id = p.id\n" +
+                        "join developers d on dp.dev_id = d.id\n" +
+                        "group by p.name_, trim(d.name_ || ' ' || d.age)\n" +
+                        "order by 1;");
+        while (resultSet.next()) {
+            System.out.println("Project name: " + resultSet.getString(1) +
+                    " ; " + "List developer: " + resultSet.getString(2));
         }
         connection.close();
     }
@@ -62,5 +89,18 @@ public class HomeWork implements Command {
         connection.close();
     }
 
-
+    private void getListDevelopers() throws SQLException { // list go
+        Connection connection = DataSourceHolder.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement
+                .executeQuery("SELECT count (dp.dev_id) || ' - ' || p.name_" +
+                        " || ' - ' || to_char (p.creation_date, 'dd.mm.yyyy') as list_project\n" +
+                        "FROM projects p\n" +
+                        "JOIN developers_projects dp on dp.projects_id = p.id\n" +
+                        "GROUP BY creation_date, p.name_;");
+        while (resultSet.next()) {
+            System.out.println("Count developer: " + resultSet.getString(1) + " ; ");
+        }
+        connection.close();
+    }
 }
