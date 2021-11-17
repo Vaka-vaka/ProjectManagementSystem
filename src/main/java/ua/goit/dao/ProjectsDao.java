4403d001e26e4168e801383d4d02d7864a652962
Vaka-vaka <Waka2006@ukr.net>
2021-11-17 12:40:24 +0200

    Update ProjectsDao.java

--- src/main/java/ua/goit/dao/ProjectsDao.java
+++ src/main/java/ua/goit/dao/ProjectsDao.java
@@ -11,7 +11,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.goit.model.Projects;
 import java.sql.*;
-import java.sql.Date;
 import java.util.*;
 
 public class ProjectsDao extends AbstractDao<Projects> {
@@ -36,14 +35,14 @@ public class ProjectsDao extends AbstractDao<Projects> {
 
     @Override
     public Optional<Projects> create(Projects entity) {
-        String sql = "insert into projects(id, name_, language, cost, creation_date)" +
+        String sql = "insert into projects(id, name_, language, creation_date, cost)" +
                 " values (?, ?, ?, ?, ?)";
         int count = DbHelper.executeWithPreparedStatement(sql, ps -> {
             ps.setLong(1, entity.getId());
             ps.setString(2, entity.getName_());
             ps.setString(3, entity.getLanguage());
-            ps.setInt(4, entity.getCost());
-            ps.setDate(5, (Date) entity.getCreation_date());
+            ps.setDate(4, (java.sql.Date) entity.getCreation_date());
+            ps.setInt(5, entity.getCost());
         });
         LOGGER.info("Created " + count + " records");
         return Optional.empty();
@@ -51,13 +50,13 @@ public class ProjectsDao extends AbstractDao<Projects> {
 
     @Override
     public void update(Projects entity) {
-        String sql = "update projects set name_ = ?, language = ?, cost = ?, " +
-                "creation_date = ? where id = ?";
+        String sql = "update projects set name_ = ?, language = ?, creation_date = ?, " +
+                "cost = ? where id = ?";
         int count = DbHelper.executeWithPreparedStatement(sql, ps -> {
             ps.setString(1, entity.getName_());
             ps.setString(2, entity.getLanguage());
-            ps.setInt(3, entity.getCost());
-            ps.setDate(4, (Date) entity.getCreation_date());
+            ps.setDate(3, (java.sql.Date) entity.getCreation_date());
+            ps.setInt(4, entity.getCost());
             ps.setLong(5, entity.getId());
         });
         LOGGER.info("Updated " + count + " records");
