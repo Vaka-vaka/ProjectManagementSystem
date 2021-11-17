de7beff90d6ea8eb94533180b23462808ba999b6
Vaka-vaka <Waka2006@ukr.net>
2021-11-17 11:06:39 +0200

    Update Command.java

--- src/main/java/ua/goit/console/Command.java
+++ src/main/java/ua/goit/console/Command.java
@@ -9,8 +9,24 @@ package ua.goit.console;
 
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public interface Command {
+    Pattern pattern = Pattern.compile("^\\w+");
 
-    void handle(String params) throws ParseException, SQLException;
+    void handle(String params, Consumer<Command> setActive) throws ParseException, SQLException;
+
+    void printActiveMenu();
+
+    default Optional<String> getCommandString(String params) {
+        Matcher matcher = pattern.matcher(params);
+        if (matcher.find()) {
+            return Optional.of(matcher.group());
+        } else {
+            return Optional.empty();
+        }
+    }
 }
